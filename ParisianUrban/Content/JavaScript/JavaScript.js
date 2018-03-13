6d757e3fb311361e445df16ca62f79e792452019
Jerome Stewart <jeromerocks@live.ca>
2018-03-13 09:22:45 -0400

    Home page started.

---
+++ ParisianUrban/Content/JavaScript/JavaScript.js
@@ -0,0 +1,6 @@
+﻿var btn = document.querySelector('.btn-white');
+
+btn.addEventListener("click", function() {
+    alert("Website under construction!");
+});
+
---
+++ ParisianUrban/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParisianUrban.Models;
+
+namespace ParisianUrban.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomeDisplay()
+        {
+            return View("HomePage");
+        }
+
+        public ActionResult AdminDisplay()
+        {
+
+            return View("AdminLogin");
+        }
+
+        [HttpPost]
+        public ActionResult AdminDisplay(string Username , string Password)
+        {
+
+            if (Username == "admin" && Password == "admin")
+            {
+                ViewBag.greeting = "Weclome";
+
+                return View("AdminLogin");
+            }
+
+            return Content("You fucked up fam");
+        }
+    }
+}
