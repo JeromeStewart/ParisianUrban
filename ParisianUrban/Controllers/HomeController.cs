7235a4c1c1a1899c970cd66206fafc9655f0e852
Jerome Stewart <jeromerocks@live.ca>
2018-03-19 17:19:22 -0400

    Added Admin table

--- ParisianUrban/Controllers/HomeController.cs
+++ ParisianUrban/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ParisianUrban.Models;
 
 namespace ParisianUrban.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities en = new Entities();
+       
         // GET: Home
         public ActionResult HomeDisplay()
         {
+   
             return View("HomePage");
         }
     
@@ -21,21 +23,48 @@ namespace ParisianUrban.Controllers
         }
 
         [HttpPost]
-        public ActionResult AdminDisplay(string Username , string Password)
+        public ActionResult AdminDisplay(int ID , string Password)
         {
-            Admin admin = new Admin();
 
-
-
-            if (Username == "admin" && Password == "admin")
+            if (ModelState.IsValid)
             {
-                admin.Name = "Jerome";
-                ViewBag.greeting = "Weclome " + admin.Name;
+
+                var query = (
+                    from db in en.Admins
+                    where db.ID == ID && db.Password == Password
+                    select new
+                    {
+                        db.ID,
+                        db.Password,
+                        db.Name
+                    }).ToList();
+
+
+
+                if (query.FirstOrDefault() != null)
+                {
+                    foreach (var item in query)
+                    {
+                        ViewBag.greeting = "Welcome " + query.FirstOrDefault().Name;
+
+                        Session["ID"] = query.FirstOrDefault().ID;
+
+                        Session["Password"] = query.FirstOrDefault().Password;
+                    }
+
+                    return View("AdminLogin");
+                }
+
+                ViewBag.greeting = "Incorrect credentials";
 
                 return View("AdminLogin");
             }
+            else
+            {
+                return View("AdminLogin");
+            }
 
-            return Content("Something went wrong :(");
         }
     }
 }
+
